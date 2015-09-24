3b0a31048508064c7f75efdd8ad521bba154c03e
Iridius <irde@mail.ru>
2015-09-24 15:27:41 +0200

    M: drawData complement tag-text but not replaces.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const _SEPARATOR = '|';
-const config = _getConfig();
+const config = _getData('http://localhost:63342/Serapeum/data/config.json');
 
 function drawData(id, data_url, display, fieldList) {
     var data = _getData(data_url);
     var result = _formatData(data, display, fieldList);
 
     var element = document.getElementById(id);
-    element.innerHTML = result;
+    element.innerHTML += result;
 }
 
 function _getData(url) {
@@ -21,41 +21,38 @@ function _getData(url) {
     return null;
 }
 
-function _getConfig() {
-    //return _getData("/data/config.json");
-    return _getData('http://localhost:63342/Serapeum/data/config.json');
-}
-
 function _formatData(data, display, fieldList) {
-    var result = '';
     switch (display) {
         case 'article':
-            result = _getContent(data)
-            break;
-        case 'blockqoute':
-            console.log(display);
+            return _getContent(data)
             break;
         case 'epigraph':
-            console.log(display);
+        case 'quote':
+            return _formatAsQuote(data);
             break;
         case 'glossary':
-            result = _formatAsGlossary(data, fieldList);
+            return _formatAsGlossary(data, fieldList);
             break;
         case 'list':
-            result = _formatAsList(data, fieldList);
-            break;
-        case 'quotes':
-            result = _formatAsQuotes(data);
+            return _formatAsList(data, fieldList);
             break;
         default: // attempt to read control type from config
+            if(!Array.isArray(data)){
+                return '';
+            }
+
+            var result = '';
             data.forEach(function(item){
                 if(item.hasOwnProperty("type")){
                     var type = item.type;
                     result += _formatData(item, type);
                 }
             });
+
+            return result;
     }
-    return result;
+
+    return '';
 }
 
 function _formatAsGlossary(data, fieldList){
@@ -80,7 +77,7 @@ function _formatAsGlossary(data, fieldList){
     return result;
 }
 
-function _formatAsQuotes(data) {
+function _formatAsQuote(data) {
     var result = '';
 
     if(data.hasOwnProperty('header')){
@@ -108,8 +105,8 @@ function _getQuoteContent(content){
         source = _format("source", content.source);
     }
 
-    if(content.hasOwnProperty('blockquote')){
-        return _format('blockquote', content.blockquote + source);
+    if(content.hasOwnProperty('quote')){
+        return _format('quote', content.quote + source);
     }
 
     return '';
@@ -141,9 +138,10 @@ function _formatAsList(data, fieldList) {
 function _format(field, value) {
     var formatting = _getChildMembers(config, "property", field)[0];
 
-    if(formatting.length === 0 || value.length === 0){
+    if(typeof formatting === "undefined" || value.length === 0){
         return value;
     }
+
     if(formatting.hasOwnProperty("tag")){
         value = "\<" + formatting["tag"] + "\>" + value + "\</" + formatting["tag"] + "\>";
     }
@@ -163,7 +161,7 @@ function _getContent(content, fieldList) {
         var field = fields[i];
         var value = "";
 
-        if(field === 'src' || field === 'group'){
+        if(field === 'src' || field === 'group' || field === 'type'){
             continue;
         }
         if(field === 'title'){
